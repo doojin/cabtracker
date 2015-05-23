426f809e552675da7aea6f9d8adcb1dfd4addcc2
Dmitry Papka <dmitry.papka@gmail.com>
2015-05-23 18:24:53 +0300

    Interactive map update. Adding filters.

--- web/assets/js/map.js
+++ web/assets/js/map.js
@@ -24,33 +24,55 @@ function initialize() {
         mapOptions
     );
 
-    new google.maps.Marker({
+    var marker1 = new google.maps.Marker({
         position: new google.maps.LatLng(56.8714744, 24.3291625),
         icon: 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png',
         map: map
     });
 
-    new google.maps.Marker({
+    var marker2 = new google.maps.Marker({
         position: new google.maps.LatLng(56.9714744, 24.1291625),
         icon: 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png',
         map: map
     });
 
-    new google.maps.Marker({
+    var marker3 = new google.maps.Marker({
         position: new google.maps.LatLng(57, 24.1291625),
         icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
         map: map
     });
 
-    new google.maps.Marker({
+    var marker4 = new google.maps.Marker({
         position: new google.maps.LatLng(56.73, 24.2291625),
         icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
         map: map
     });
 
-    new google.maps.Marker({
+    var marker5 = new google.maps.Marker({
         position: new google.maps.LatLng(56.81, 24.1791625),
         icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
         map: map
     });
+
+    var infowindow = new google.maps.InfoWindow({
+        content: infoWindowContent
+    });
+
+    google.maps.event.addListener(marker1, 'click', function() {
+        infowindow.open(map, marker1);
+    });
+    google.maps.event.addListener(marker2, 'click', function() {
+        infowindow.open(map, marker2);
+    });
+    google.maps.event.addListener(marker3, 'click', function() {
+        infowindow.open(map, marker3);
+    });
+    google.maps.event.addListener(marker4, 'click', function() {
+        infowindow.open(map, marker4);
+    });
+    google.maps.event.addListener(marker5, 'click', function() {
+        infowindow.open(map, marker5);
+    });
+
+
 }
